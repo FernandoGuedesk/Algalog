bb7a82f9f4d3ee0e308cf5e52bd0cd6b2a4484e0
Fernando Guedes <nando3ryu@gmail.com>
2022-12-21 16:47:18 -0300

    Teste classe principal

--- src/test/java/com/algaworks/algalog/AlgalogApiApplicationTests.java
+++ src/test/java/com/algaworks/algalog/AlgalogApiApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AlgalogApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		AlgalogApiApplication.main(new String[] {});
 	}
 
 }
